2154e8e638f7b19329514bf6f4457c5d35f0aba7
ANKITpandey75 <125044396+ANKITpandey75@users.noreply.github.com>
2026-06-29 11:10:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-//your JS code here. If required.
+var element = document.getElementById("level");
+var level = 1; 
+
+while (element.parentElement) {
+    element = element.parentElement;
+    level++;
+}
+
+alert("The level of the element is: " + level);//your JS code here. If required.
